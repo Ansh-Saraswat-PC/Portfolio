4fe54d6d0a8cbe8d1e41143ecd709bc8d5d52f43
Ansh Saraswat <the.glorious.curassier@gmail.com>
2026-01-24 12:47:06 +0530

    fixed configg file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,9 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://anshsaraswat.dev', // <--- This MUST match your new domain
   integrations: [tailwind(), react()],
-  vite: {
-    resolve: {
-      alias: {
-        "@": "/src",
-        "@components": "/src/components",
-      },
-    },
-  },
-  output: "static",
-  build: {
-    inlineStylesheets: "auto",
-  },
-  server: {
-    host: true,
-    port: 4321,
-  },
 });
